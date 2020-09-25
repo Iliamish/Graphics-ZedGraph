085f372e6df6871178b08798fb78df5348f77a88
MADZEROPIE <ask_ii1@mail.ru>
2020-09-25 17:26:23 +0300

    fixed bug

--- numerical_methods.h
+++ numerical_methods.h
@@ -57,7 +57,7 @@ std::vector<std::pair<double, double> > RungeKutta4 //Метод РГ 4 порядка
 			auto p1 = RK4_new_point(f, x, y, h);
 			auto p12 = RK4_new_point(f, x, y, h / 2.0);
 			auto p2 = RK4_new_point(f, p12.first, p12.second, h / 2);
-			double s = (p2.second - p1.second) / 15;
+			double s = abs(p2.second - p1.second) / 15;
 			if (s > eps) h = h / 2;
 			else {
 				x = p1.first; y = p1.second;
